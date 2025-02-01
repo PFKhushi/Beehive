98c18c1de4da4dbd4f5e2646d4e3e9bff60182aa
Pedro Firmino <mafiazuka@gmail.com>
2025-02-01 05:55:59 -0300

    started classes for types of bees and the hive

---
+++ Bee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beehive
+{
+    internal class Bee
+    {
+        string job;
+        protected virtual float CostPerShift { get; set; }
+
+        public void WorkNextShift()
+        {
+
+        }
+        public virtual void DoJob()
+        {
+
+        }
+        
+
+    }
+    class Queen : Bee
+    {
+        protected override float CostPerShift { get; set; }
+
+        public void AssignBee() 
+        {
+            
+        }
+        public void LayEggs() 
+        {
+        }
+        public override void DoJob()
+        {
+
+        }
+
+
+    }
+    class EggCare : Bee
+    {
+        public override void DoJob() 
+        { 
+        
+        }
+        
+    }
+    class HoneyMaking : Bee
+    {
+        public override void DoJob()
+        {
+
+        }
+    }
+    class NectarCollection : Bee
+    {
+        public override void DoJob()
+        {
+
+        }
+    }
+}
---
+++ Hive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beehive
+{
+    internal class Hive
+    {
+        private float honey = 25f;
+        private float nectar = 100f;
+        string statusReport { get; }
+
+        public void CollectNectar(int amount)
+        {
+            honey += amount;
+        }
+
+        public void NectarToHoney()
+        {
+            nectar -= 1f;
+            honey += 1.2f;
+        }
+        public bool ConsumeHoney(int amount)
+        {
+            if (amount < honey) 
+            {
+                honey -= amount;
+                return true;
+            }
+            return false;
+        }
+    }
+}
